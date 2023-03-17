fce91c55f34cff52712b3f0b80f29defb53fca26
anurag-int <ag6526803@gmail.com>
2023-03-17 15:52:23 +0530

    js updated

---
+++ app.js
@@ -0,0 +1,46 @@
+const btnEl = document.getElementById("button");
+const errorMessageEl = document.getElementById("errorMessage");
+const galleryEl = document.getElementById("gallery");
+
+async function fetchImage() {
+  const inputValue = document.getElementById("input").value;
+
+  if (inputValue > 10 || inputValue < 1) {
+    errorMessageEl.style.display = "block";
+    errorMessageEl.innerText = "Number should be between 0 and 11";
+    return;
+  }
+
+  imgs = "";
+
+  try {
+    
+    await fetch(
+      `https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(
+        Math.random() * 1000
+      )}&client_id=KThOQpZswvKhcZAoKpZL1PG44y1whvHJrMzxeff8-9s`
+    ).then((res) =>
+      res.json().then((data) => {
+        if (data) {
+          data.forEach((pic) => {
+            imgs += `
+            <img src=${pic.urls.small} alt="image"/>
+            `;
+            galleryEl.style.display = "block";
+            galleryEl.innerHTML = imgs;
+            btnEl.style.display = "block";
+            errorMessageEl.style.display = "none";
+          });
+        }
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    errorMessageEl.style.display = "block";
+    errorMessageEl.innerHTML = "An error happened, try again later";
+    btnEl.style.display = "block";
+    galleryEl.style.display = "none";
+  }
+}
+
+btnEl.addEventListener("click", fetchImage);
